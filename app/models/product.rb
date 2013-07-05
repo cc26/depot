6343538e948653d880e3c2b784002bcd06ea43ea
cc26 <reynold8002@gmail.com>
2013-07-04 22:21:29 -0400

    add quantities to smart cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,13 +6,13 @@ class Product < ActiveRecord::Base
   
   attr_accessible :description, :image_url, :price, :title
   # validates the presence 
-  validates :title, :price, :description, :presence => true
+  validates :title, :price, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
   validates :title, :uniqueness => true
-  validates :image_url, :format => {
-  	:with => %r{\.(gif|jpg|png)}i,
-  	:message => 'must be a URL for GIF, JPG or PNG image.'
-  }
+  # validates :image_url, :format => {
+  # 	:with => %r{\.(gif|jpg|png)}i,
+  # 	:message => 'must be a URL for GIF, JPG or PNG image.'
+  # }
 
   # def ensure_not_referenced_by_any_line_item
   #   if line_items.count.zero?
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+products = [
+  {title:'Monkey',price: 100},
+  {title:'Elephant',price: 100},
+  {title:'King Kong',price: 100},
+  {title:'Stupid ass',price: 100},
+  {title:'Hello Kitty',price: 100},
+  {title:'This is a book',price: 100}
+]
+
+products.each do |product|
+  Product.create!(product)
+end
